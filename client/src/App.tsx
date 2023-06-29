b9297dd84fce76303008b97d90e98d0949a5a7d4
dpitt <dpitt@keyholesoftware.com>
2023-06-29 13:10:02 -0500

    upated select

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
-import React, { useState, ChangeEvent } from 'react';
+import React, { useState, ChangeEvent, useEffect } from 'react';
 import axios from 'axios';
 import Spinner from './components/Spinner';
 import { isCodeDetected } from './helpers/codeDetection';
 import { FormattedMessage, FormattedListMessage } from './components/FormattedMessage';
 import { MdDelete, MdChatBubbleOutline } from "react-icons/md";
+import CSS from 'csstype';
+
 
 interface IMessage {
   text: string;
@@ -14,25 +16,61 @@ interface IMessage {
 }
 
 function App(): JSX.Element {
+
+
+  const load =() => {
+
+        let s : string | null  = localStorage.getItem("chats");
+
+        
+        if (s) {
+         let a : IMessage[] | null  = JSON.parse( s  );
+         if (a) {
+           return a;
+         }
+        }
+       
+        return [];
+  }
+
+
   const [loading, setLoading] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(-1);
   const [chatResult, setChatResult] = useState<string>('');
   const [chatInput, setChatInput] = useState<string>('');
   const [chatPrompt, setChatPrompt] = useState<string>('');
-  const [messages, setMessages] = useState<IMessage[]>([]);
+  const [messages, setMessages] = useState<IMessage[]>(load());
   const messagesEndRef = React.useRef<HTMLDivElement>(null); // Define a ref for the end of the messages list
+  const divref = React.useRef<HTMLDivElement>(null);
 
 
   const generateKey = (pre: string) => {
     return `${pre}_${new Date().getTime()}`;
   }
 
-  const select = (user: string, response: string) => {
+  const select = (index: number, user: string, response: string) => {
 
     setChatInput(user);
     setChatResult(response);
 
+    const element = divref.current;
+
+    let ele = element?.getAttribute("id");
+    console.log("Clicked...."+ele);
+    if (ele) {
+      setSelectedIndex(index);
+    }
+  
+
   }
 
+
+  const save = () => {
+      localStorage.setItem("chats",JSON.stringify(messages) );
+  }
+
+ // load user chats from local storage
+ // useEffect( ()=> load());
 
   const handleInputChange = (event: ChangeEvent<HTMLTextAreaElement>): void => {
     const { value } = event.target;
@@ -57,15 +95,8 @@ function App(): JSX.Element {
 
     try {
 
-
-      //   if (chatInput != '') {
-      //   setMessages((prevMessages) => [...prevMessages, { text: chatInput, response: chatResult, isUser: true }]);
-      //  }
-
-
       setChatInput(chatPrompt);
       setChatResult("");
-
 
       // Port 5001 should match the API_PORT in .env file.
       const response = await axios.post('http://localhost:5001/createChatCompletion', {
@@ -73,20 +104,19 @@ function App(): JSX.Element {
       });
 
 
-
       setMessages((prevMessages) => [...prevMessages, { text: chatPrompt, response: response.data.message, isUser: true }]);
-
-
-
+     
       setChatResult(response.data.message);
+
+      setSelectedIndex(selectedIndex + 1);
 
 
       // Reset chat prompt
       setChatPrompt('');
 
-
-
       setLoading(false);
+
+      save();
 
     } catch (error) {
       console.error('Failed to generate response:', error);
@@ -102,9 +132,11 @@ function App(): JSX.Element {
       // Scroll smoothly to the end of the messages list
       messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
     }
+    save();
   }, [messages]); // Only re-run the effect if messages changes
 
 
+  
   return (
     <React.Fragment>
       <div className="header">
@@ -116,13 +148,15 @@ function App(): JSX.Element {
 
 
               <div className="response-select">
-                <div onClick={() => select(message.text, message.response)}
-                  key={"userlist" + index}
-                  className='user-message'>
+                <div 
+                  ref={divref}
+                  id={""+index}
+                  onClick={() => select(index,message.text, message.response)}
+                  key={index}
+                  className={index == selectedIndex ? 'selected-message' :'user-message'}>
+                   
                   <FormattedListMessage text={message.text} />
                 </div>
-
-
 
               </div>
 
