89e39705e700c6112670ba2f2eacee5f0135b9a9
dpitt <dpitt@keyholesoftware.com>
2023-07-21 11:49:52 -0500

    user login

--- client/src/Login.tsx
+++ client/src/Login.tsx
@@ -2,46 +2,81 @@ import React, { useState, ChangeEvent } from 'react';
 import axios from 'axios';
 import Spinner from './components/Spinner';
 
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Login(): JSX.Element {
 
     const [userid, setUserid] = useState<string>('');
     const [password, setPassword] = useState<string>('');
+    const [error, setError] = useState<string>('');
+    const navigate = useNavigate();
 
+    
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
 
-    const Userid = 'User Id';
-    const handleLogin = async (): Promise<void> => {
+        const params = new URLSearchParams();
+        params.append('username', userid);
+        params.append('password', password);
+
+        // Port 5001 should match the API_PORT in .env file.
+        const response = await axios.post('http://localhost:5001/login', params);
+
+        if (response.status == 200) {
+
+            navigate('/chat', { replace: true, state: { userid: userid } })
+
+        } else {
+
+            setError("Invalid Login Credentials");
+
+        }
 
     };
 
 
-     // Define a function to handle input changes
-  const handleUserIdChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setUserid(event.target.value);
-   
-  };
+    // Define a function to handle input changes
+    const handleUserIdChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUserid(event.target.value);
+
+    };
+
+    // Define a function to handle input changes
+    const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.target.value);
+
+    };
+
 
 
     return (
         <React.Fragment>
 
-                <div className="input-container">
+            <div className="input-container">
 
-                <div className="login"> 
+                <form onSubmit={handleSubmit}>
+                    <div className="login">
 
-                    <h2> Login Screen  </h2>
+                        <h2> Login Screen  </h2>
 
-                    <span> User Id: <input width={50} value={userid} onChange={handleUserIdChange}     />  </span> <br />
-                     <br />
-                    <span> Password: <input type="password" width={50} /> </span> <br />
+                        <span> User Id: <input width={50} id="username" name="username" value={userid} onChange={handleUserIdChange} />  </span> <br />
+                        <br />
+                        <span> Password: <input type="password" id="password" name="password" width={50} onChange={handlePasswordChange} /> </span> <br />
 
-                     <br />
-                    <Link to='/chat' state={{ userid: userid}} >
-                        <button>Login</button>
-                    </Link>
+                        <br />
 
-                </div>
+                        <button type="submit">Submit</button>
+                        <Link to='/chat' state={{ userid: userid }} >
+                            <button>Login</button>
+                        </Link>
+
+                        <br />
+
+                        <h3>{error}</h3>
+
+                    </div>
+
+                </form>
 
             </div>
 
